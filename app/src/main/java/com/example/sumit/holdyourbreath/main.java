1e6bd04b80827efb2b1f2631a777bfbcfc3a5307
sumitPhD <agrawalsumit1989@gmail.com>
2020-04-03 23:55:46 +0530

    save the chronometer time, after clicking the pause button. (and showing it on screen)

--- app/src/main/java/com/example/sumit/holdyourbreath/main.java
+++ app/src/main/java/com/example/sumit/holdyourbreath/main.java
@@ -5,6 +5,8 @@ import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import static com.example.sumit.holdyourbreath.R.id.chronometer1;
 
@@ -12,15 +14,29 @@ public class main extends AppCompatActivity {
     private Chronometer chronometerB;
     public boolean running;
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Chronometer chronometerB =(Chronometer) findViewById(chronometer1);
-        //chronometer.start();
+        //Displaying Toast with Hello Javatpoint message
+        Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_LONG).show();
 
+        Chronometer chronometerB = (Chronometer) findViewById(chronometer1);
+
+        //chronometer.start();
     }
+
+
+
+    private void showElapsedTime() {
+            long elapsedMillis = SystemClock.elapsedRealtime() - ((Chronometer) findViewById(chronometer1)).getBase();
+            Toast.makeText(main.this, "Elapsed milliseconds: " + elapsedMillis,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+
     /** Called when the user touches the button */
     public void StartChronometer(View v)
     {
@@ -39,14 +55,26 @@ public class main extends AppCompatActivity {
             // ((Chronometer) findViewById(chronometer1)).stop();
             ((Chronometer) findViewById(chronometer1)).stop();
             running=false;
+            long elapsedMillis = SystemClock.elapsedRealtime() - ((Chronometer) findViewById(chronometer1)).getBase();
+            int seconds = (int)(elapsedMillis/1000 % 60);
+            //Intent intent = new Intent(getApplicationContext(),
+              //      main.class).putExtra("timer",seconds);
+            //startActivity(intent);
+            display(seconds);
+
         }
 
 
     }
+    private void display(int seconds) {
+        TextView quantityTextView = (TextView) findViewById(R.id.textTime);
+        quantityTextView.setText(String.valueOf(seconds));}
 
-    public void resetChronometer(View v)
-    {
-                   ((Chronometer) findViewById(chronometer1)).setBase(SystemClock.elapsedRealtime());
-        running=false;
-            }
+    public void resetChronometer(View v) {
+        ((Chronometer) findViewById(chronometer1)).setBase(SystemClock.elapsedRealtime());
+        running = false;
+    }
 }
+
+
+
