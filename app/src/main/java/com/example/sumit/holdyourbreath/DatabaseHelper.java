26f140fc617986dc7932c913e7f52bd20ea000c3
sumitPhD <agrawalsumit1989@gmail.com>
2020-04-04 02:50:36 +0530

    Using SQL making a table for 3 breaths

---
+++ app/src/main/java/com/example/sumit/holdyourbreath/DatabaseHelper.java
@@ -0,0 +1,54 @@
+package com.example.sumit.holdyourbreath;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by SUMIT on 4/4/2020.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "breath.db";
+    public static final String TABLE_NAME = "breath_table";
+    public static final String COL1 = "ID";
+    public static final String COL2 = "Breath1";
+    public static final String COL3 = "Breath2";
+    public static final String COL4 = "Breath3";
+
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        //SQLiteDatabase db = this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT,Breath1 INTEGER,Breath2 INTEGER,Breath3 INTEGER)");
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String breath1,String breath2,String breath3 ){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2, breath1);
+        contentValues.put(COL3, breath2);
+        contentValues.put(COL4, breath3);
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if (result==-1)
+            return false;
+        else
+                return true;
+
+    }
+}
--- app/src/main/java/com/example/sumit/holdyourbreath/main.java
+++ app/src/main/java/com/example/sumit/holdyourbreath/main.java
@@ -4,27 +4,37 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import static com.example.sumit.holdyourbreath.R.id.chronometer1;
 
 public class main extends AppCompatActivity {
+    DatabaseHelper myDb;
     private Chronometer chronometerB;
     public boolean running;
-
+    EditText editText_breath1,editText_breath2,editText_breath3;
+    Button btn_addData;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
+
+        editText_breath1 = (EditText) findViewById(R.id.editText_breath1);
+        editText_breath2 = (EditText) findViewById(R.id.editText_breath2);
+        editText_breath3 = (EditText) findViewById(R.id.editText_breath3);
+        btn_addData = (Button)findViewById(R.id.btn_addData);
 
         //Displaying Toast with Hello Javatpoint message
         Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_LONG).show();
 
         Chronometer chronometerB = (Chronometer) findViewById(chronometer1);
-
+        AddData();
         //chronometer.start();
     }
 
@@ -74,7 +84,30 @@ public class main extends AppCompatActivity {
         ((Chronometer) findViewById(chronometer1)).setBase(SystemClock.elapsedRealtime());
         running = false;
     }
+
+    public void AddData(){
+        btn_addData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                     boolean isInserted =    myDb.insertData(editText_breath1.getText().toString(),
+                             editText_breath2.getText().toString(),
+                             editText_breath3.getText().toString());
+                        if (isInserted=true)
+                            Toast.makeText(main.this,"Data Insterted", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(main.this,"Data is not Insterted", Toast.LENGTH_LONG).show();
+
+                    }
+                }
+        );
+    }
+
+
+
 }
+
+
 
 
 
